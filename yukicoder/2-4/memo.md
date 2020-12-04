8424661ddb95e351300b1c3bcf470bf092b7cc89
ynishimura <ynishimura0922@gmail.com>
2020-12-04 10:23:21 +0900

    add 2-4

---
+++ yukicoder/2-4/memo.md
@@ -0,0 +1,6 @@
+## 概要
+
+動的計画法の勉強にもってこいな問題
+
+numpy でもできそうな気がする
+https://yukicoder.me/submissions/423657
--- yukicoder/2-4/python/main.py
+++ yukicoder/2-4/python/main.py
@@ -1,4 +1,52 @@
 N = int(input()) # おもりの数
-W = list(map(int, input().split())) # おもりの重さ(半角スペース区切り)
+Weight = list(map(int, input().split())) # おもりの重さ(半角スペース区切り)
 
-# 全てのおもりを使って水平 => 片方に全重さ/2となる
+total = sum(Weight)
+half = total // 2
+# possibleかimpossibleを返すだけなので、重さを気にする必要ない
+# 全てのおもりを使って天秤が水平 => 片方に全重さ/2となるか判定
+
+# おもり(100以下の重さのおもりが100個以下)
+
+'''
+
+重りの重さの合計が奇数のときimpossible
+
+
+'''
+
+# weight_sum = sum(W)
+# weight_sum_half = weight_sum / 2
+
+def check():
+    if sum(Weight) % 2 != 0:
+        print("impossible")
+    else:
+        half = sum(Weight) // 2
+        '''
+        # ex) 1 2 3
+        # 重りの合計/2 = 3
+        # 縦(重りの数) x 横(重りの合計/2)
+        [[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0]]
+         
+        #dp[重りの数][重りの合計/2] : i番目まで見た時に重さjを作ることができるか判定していく
+
+        '''
+        dp = [[0 for i in range(half + 1)] for j in range(N + 1)]
+        dp[0][0] = 1 # おもりを使わないとき
+        for i in range(N):
+            for j in range(half + 1):
+                # 重りの重さ <= 半分の重さ
+                if Weight[i] <= j:
+                    dp[i + 1][j] = dp[i][j - Weight[i]] or dp[i][j]
+                else:
+                    dp[i + 1][j] = dp[i][j] 
+            # 
+    print(sum(Weight), sum(Weight) // 2)
+
+
+if __name__ == "__main__":
+    check()
