9fa29f5661d756e3e95c8430f346c43ef9de3b26
ynishimura <ynishimura0922@gmail.com>
2020-12-10 16:15:41 +0900

    とりあえずできたのでアップ

--- yukicoder/2-4/python/main.py
+++ yukicoder/2-4/python/main.py
@@ -1,51 +1,57 @@
-N = int(input()) # おもりの数
-Weight = list(map(int, input().split())) # おもりの重さ(半角スペース区切り)
+import sys
 
-total = sum(Weight)
-half = total // 2
+N = int(input()) # おもりの数
+weight_list = list(map(int, input().split())) # おもりの重さ(半角スペース区切り)
+
+weight_sum = sum(weight_list)
+half = weight_sum // 2
 # possibleかimpossibleを返すだけなので、重さを気にする必要ない
 # 全てのおもりを使って天秤が水平 => 片方に全重さ/2となるか判定
-
 # おもり(100以下の重さのおもりが100個以下)
 
-'''
-
-重りの重さの合計が奇数のときimpossible
-
-
-'''
-
-# weight_sum = sum(W)
-# weight_sum_half = weight_sum / 2
-
 def check():
-    if sum(Weight) % 2 != 0:
+    if weight_sum % 2 != 0: # おもりの重さの合計が奇数のときimpossible
         print("impossible")
+        sys.exit(0) # 終了
     else:
-        half = sum(Weight) // 2
         '''
         # ex) 1 2 3
         # 重りの合計/2 = 3
-        # 縦(重りの数) x 横(重りの合計/2)
-        [[0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0]]
+        # 縦(重りの数)i x 横(重りの合計/2)j
+        [[True, False, False, False],
+         [True, False, False, False],
+         [True, False, False, False],
+         [True, False, False, False]]
          
-        #dp[重りの数][重りの合計/2] : i番目まで見た時に重さjを作ることができるか判定していく
+        #dp[重りの数(i)][(重りの合計/2)までの数字(j)] : i番目まで見た時に重さjを作ることができるか判定していく
 
         '''
-        dp = [[0 for i in range(half + 1)] for j in range(N + 1)]
-        dp[0][0] = 1 # おもりを使わないとき
+        dp = [[True] + [False]*(half + 1) for i in range(N)] # 最初はTrue,
         for i in range(N):
             for j in range(half + 1):
-                # 重りの重さ <= 半分の重さ
-                if Weight[i] <= j:
-                    dp[i + 1][j] = dp[i][j - Weight[i]] or dp[i][j]
-                else:
-                    dp[i + 1][j] = dp[i][j] 
-            # 
-    print(sum(Weight), sum(Weight) // 2)
+                # dp[i番目までのおもりを見た時][左側の重さj]
+                # i番目のおもりの時に,左側の重さがjが存在するか判定
+                # j - weight_list[i]
+                # 重りの合計/2(j) から 対象の重りの重さ(weight_list[i])　を　引く
+
+                if dp[i-1][j]: # 一つ前の判定でTrue
+                    dp[i][j] = True # 組み合わせが存在する
+                    continue
+                if j - weight_list[i] < 0: # 作りたい重さよりおもりが重い => 作れない
+                    # omori_now = weight_list[i]
+                    # omori_sum_yoko = j
+                    # omori_kazu_tate = i
+                    continue
+                if dp[i-1][j - weight_list[i]]: # 作りたい重さで足りなかった分の重さは作れるか
+                    # omori_now = weight_list[i]
+                    # omori_sum_yoko = j
+                    # omori_kazu_tate = i
+                    dp[i][j] = True
+
+    if dp[-1][weight_sum//2]:
+        print('possible')
+    else:
+        print('impossible')
 
 
 if __name__ == "__main__":
