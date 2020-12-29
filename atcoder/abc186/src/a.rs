bf616e94926a262c59df8abb11e8930bd423dcfc
ynishimura <ynishimura0922@gmail.com>
2020-12-29 23:53:58 +0900

    abc186

---
+++ atcoder/abc186/src/a.rs
@@ -0,0 +1,19 @@
+#![allow(unused_imports)]
+use num::{
+    abs,
+    integer::{gcd, ExtendedGcd},
+};
+use permutohedron::LexicalPermutation;
+use proconio::{fastout, input, marker::*};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+use text_io::read;
+
+#[fastout]
+fn main() {
+    input! {
+        n: i32,
+        w: i32,
+    }
+
+    println!("{}", n / w);
+}
