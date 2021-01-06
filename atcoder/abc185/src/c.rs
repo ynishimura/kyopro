79a68fee306c0402e11dd3bf1ec772d5981d9e41
ynishimura <ynishimura0922@gmail.com>
2021-01-06 23:26:59 +0900

    add c

--- atcoder/abc185/src/c.rs
+++ atcoder/abc185/src/c.rs
@@ -1,2 +1,22 @@
 // https://atcoder.jp/contests/abc185/tasks/abc185_c
 // Duodecim Ferra
+
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        l: u128,
+    }
+
+    let mut ans = 1;
+    // ans:  combination(l-12+11, 11)
+    for i in 1..=11 {
+        ans *= l - i;
+    }
+    for i in 1..=11 {
+        ans /= i;
+    }
+
+    println!("{}", ans);
+}
