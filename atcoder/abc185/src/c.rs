255ac6d033c0adbb2d4f6a767a84974897a5162e
ynishimura <ynishimura0922@gmail.com>
2021-01-06 23:30:39 +0900

    refactor

--- atcoder/abc185/src/c.rs
+++ atcoder/abc185/src/c.rs
@@ -12,9 +12,9 @@ fn main() {
     let mut ans = 1;
     // ans:  combination(l-12+11, 11)
     for i in 1..=11 {
+        // 分子の計算
         ans *= l - i;
-    }
-    for i in 1..=11 {
+        // 分母の計算
         ans /= i;
     }
 
