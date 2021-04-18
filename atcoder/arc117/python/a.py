8a8f8f8046e78105a710c09700fe9e90badfb113
ynishimura <ynishimura0922@gmail.com>
2021-04-18 23:05:45 +0900

    add a

--- atcoder/arc117/python/a.py
+++ atcoder/arc117/python/a.py
@@ -9,3 +9,25 @@ b個の負の整数を作成
 -1
 [1, -1]
 """
+
+res = []
+if(a == b):
+    for i in range(1, a+1):
+        res.append(i)
+        res.append(-i)
+elif(a > b):
+    res.extend(range(1, a+1))
+    for i in range(1, b+1):
+        if(i == b):
+            res.append(-sum(res))
+        else:
+            res.append(-i)
+elif(b > a):
+    res.extend(range(-1, -(b+1), -1))
+    for i in range(1, a+1):
+        if(i == a):
+            res.append(-sum(res))
+        else:
+            res.append(i)
+
+print(*res)
