138ba847c678eac170070e9dee1d4639779ca69c
ynishimura <ynishimura0922@gmail.com>
2021-05-16 22:22:23 +0900

    add sample

--- leetcode/wc241/python/5759.py
+++ leetcode/wc241/python/5759.py
@@ -18,30 +18,37 @@ from typing import List
 
 
 class Solution:
-    def subsetXORSum(self, nums: List[int]) -> int:
-        init = 0
-        total = 0
-        counter = 0
-        while counter < len(nums):
-            list = []
-            for i, num in enumerate(nums):
-                if(i <= counter):
-                    list.append(num)
-                    print(list)
-                    xor = 0
-                    if(len(list) <= 1):
-                        xor = 0 ^ num
-                    else:
-                        xor = init ^ num
-                        print(f'{init}^{num}={xor}')
-                    total += xor
-                    init = num
+    def subsetXORSum(self, A: List[int]) -> int:
+        # init = 0
+        # total = 0
+        # counter = 0
+        # while counter < len(nums):
+        #     list = []
+        #     for i, num in enumerate(nums):
+        #         if(i <= counter):
+        #             list.append(num)
+        #             print(list)
+        #             xor = 0
+        #             if(len(list) <= 1):
+        #                 xor = 0 ^ num
+        #             else:
+        #                 xor = init ^ num
+        #                 print(f'{init}^{num}={xor}')
+        #             total += xor
+        #             init = num
 
-            counter += 1
-        print(f'===> {total}')
+        #     counter += 1
+        # print(f'===> {total}')
+        def go(i=0, x=0):
+            if i == len(A):
+                return x
+            include = go(i + 1, x ^ A[i])  # ✅ case 1: include
+            exclude = go(i + 1, x)         # 🚫 case 2: exclude
+            return include + exclude
+        return go()
 
 
 sol = Solution()
-sol.subsetXORSum([1, 3])
-sol.subsetXORSum([5, 1, 6])
-sol.subsetXORSum([3, 4, 5, 6, 7, 8])
+print(sol.subsetXORSum([1, 3]))
+print(sol.subsetXORSum([5, 1, 6]))
+print(sol.subsetXORSum([3, 4, 5, 6, 7, 8]))
