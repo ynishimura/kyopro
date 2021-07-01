deaecdf60a8c5860be5a391d923e4ce7c06520ea
ynishimura <ynishimura0922@gmail.com>
2021-07-01 23:47:01 +0900

    add leetcode

---
+++ leetcode/1.two-sum.py
@@ -0,0 +1,25 @@
+# @before-stub-for-debug-begin
+from python3problem1 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, value in enumerate(nums):
+            remaining = target - nums[i]
+            if remaining in seen:
+                return [i, seen[remaining]]
+            else:
+                seen[value] = i
+
+# @lc code=end
